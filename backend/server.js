9c20b092d811984ff237a6fc3c6554e3d6d947e3
Sukrit Ghosh <sukritghosh03@gmail.com>
2024-08-23 15:17:57 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -16,6 +16,8 @@ dotenv.config();
 // const app =express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -23,6 +25,12 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // app.get("/", (req,res) => {
 //     res.send("Hello world");
